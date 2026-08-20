2d32c1bf4d9762d618482a1f066ae75a38352c7e
Severjan Lici <s338821@studenti.polito.it>
2026-08-20 18:21:58 +0200

    "mobile view"

--- components/project.tsx
+++ components/project.tsx
@@ -25,11 +25,11 @@ export function Project({ title, description, tags, imageUrl }: projectProps) {
         }}
         className='group mb-3 sm:mb-8 last:mb-0 '>
     <section 
-        className=" bg-gray-100 max-w-[42rem] border border-black/5 
-                    overflow-hidden sm:pr-8 relative sm:h-[20rem]
+        className="flex flex-col bg-gray-100 max-w-[42rem] border border-black/5 
+                    overflow-hidden md:pr-8 relative md:h-[20rem]
                     rounded-lg
-                    hover:bg-gray-200 transition group-even:pl-8">
-    <div className="py-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-1 sm:max-w-[50%] flex flex-col h-full group-even:ml-auto group-even:sm:pl-2 group-even:sm:pr-10">
+                    hover:bg-gray-200 transition md:group-even:pl-8">
+      <div className="order-2 py-4 pb-7 px-5 md:order-none md:pl-10 md:pr-2 md:pt-1 md:max-w-[50%] flex flex-col h-full md:group-even:ml-auto md:group-even:pl-2 md:group-even:pr-10">
       <h3 className="text-2xl font-semibold">{title}</h3>
     <p className="mt-2 leading-relaxed text-gray-700 ">{description}</p>
     <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">{
@@ -39,16 +39,16 @@ export function Project({ title, description, tags, imageUrl }: projectProps) {
       ))}</ul>
     </div>
       <Image src={imageUrl} alt={"Projects i worked on"} quality={93} 
-      className="absolute top-8 -right-40 w-[28.25rem] 
-                rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 
-                goup-hover:-translate-x-3 transition 
-                group:hover:-translate-y-3 
-                group-hover:-rotate-2
-                group-hover:scale-[1.04]  
+      className="order-1 relative top-auto right-auto left-auto w-full h-auto 
+            rounded-t-lg shadow-2xl md:absolute md:top-8 md:-right-40 md:w-[28.25rem] 
+            md:group-even:-right-[initial] md:group-even:-left-40 
+              md:transition 
+              md:group-hover:-translate-y-3 
+              md:group-hover:-rotate-2
+              md:group-hover:scale-[1.04]  
                 
-                group-even:goup-hover:translate-x-3 
-                group-even:group:hover:translate-y-3 
-                group-even:group-hover:rotate-2"/>
+              md:group-even:group-hover:translate-y-3 
+              md:group-even:group-hover:rotate-2"/>
   </section> 
   </motion.div>
 } 
